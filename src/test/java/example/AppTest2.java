56df0047795c09c0d734733b2f406c07c3e515e8
Ashish <ash1174@gmail.com>
2017-12-05 20:36:53 +0530

    Update AppTest2.java

--- src/test/java/example/AppTest2.java
+++ src/test/java/example/AppTest2.java
@@ -3,7 +3,7 @@ package example;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
  
-public class AppTest {
+public class AppTest2 {
 
       public void testSlow() {
         System.out.println("slow");
